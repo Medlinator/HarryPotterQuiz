376dc79edef4c211e8df3bced984b972fe9f1b55
Sean Medlin <sean.medlin@yahoo.com>
2018-01-25 09:20:00 -0600

    Update app to show comments and share intent

--- app/src/main/java/com/example/android/harrypotterquiz/MainActivity.java
+++ app/src/main/java/com/example/android/harrypotterquiz/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.android.harrypotterquiz;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -15,6 +17,9 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * Declaring each variable globally so they can be used in multiple methods
+     */
     RadioButton questionOneRadioButtonA;
     RadioButton questionOneRadioButtonB;
     RadioButton questionOneRadioButtonC;
@@ -51,6 +56,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         * Set global variables to ids in activity_main.xml on app creation
+         */
         Typeface font = ResourcesCompat.getFont(this, R.font.harry_potter);
         questionOneRadioButtonA = (RadioButton) findViewById(R.id.question_one_radio_button_a);
         questionOneRadioButtonA.setTypeface(font);
@@ -111,8 +119,13 @@ public class MainActivity extends AppCompatActivity {
         questionTenEditText = (EditText) findViewById(R.id.question_ten_edit_text);
     }
 
+    /**
+     * After the submit button is clicked, the following method will tally up the user's score and give an appropriate toast
+     */
     public void submitQuiz(View view) {
-        Toast toast;
+        /**
+         * The following variables check to see if the correct answer was selected by the user. True if selected, false if not
+         */
         boolean questionOneRightAnswer = questionOneRadioButtonC.isChecked();
         boolean questionTwoRightAnswer = questionTwoRadioButtonB.isChecked();
         boolean questionThreeRightAnswer = questionThreeRadioButtonC.isChecked();
@@ -127,22 +140,48 @@ public class MainActivity extends AppCompatActivity {
         boolean questionNineRightAnswer = questionNineRadioButtonA.isChecked();
         String questionTenAnswer = questionTenEditText.getText().toString();
 
+        /**
+         * Declaring a score variable to store the user's score from the calculateScore method
+         */
         int score = calculateScore(questionOneRightAnswer, questionTwoRightAnswer, questionThreeRightAnswer, questionFourRightAnswer, questionFiveRightAnswerOne, questionFiveWrongAnswerOne, questionFiveWrongAnswerTwo, questionFiveRightAnswerTwo, questionSixRightAnswer, questionSevenRightAnswer, questionEightRightAnswer, questionNineRightAnswer, questionTenAnswer);
 
+        /**
+         * If the user scores a perfect 100, the following toast will appear on screen
+         */
         if (score == 100) {
-            toast = Toast.makeText(getApplicationContext(), "You scored a " + score + "\nCongratulations Muggle!", Toast.LENGTH_LONG);
-            toast.show();
-        } else if (score >= 70) {
-            toast = Toast.makeText(getApplicationContext(), "You scored a " + score + "\nNot bad!", Toast.LENGTH_LONG);
-            toast.show();
-        } else {
-            toast = Toast.makeText(getApplicationContext(), "You scored a " + score + "\nTry again Mudblood.", Toast.LENGTH_LONG);
+           Toast toast = Toast.makeText(getApplicationContext(), "You scored a " + score + "\nCongratulations Muggle!", Toast.LENGTH_LONG);
             toast.show();
         }
+            /**
+             * If the user scores >= 70 < 100, the following toast will appear on screen
+             */
+            else if (score >= 70) {
+            Toast toast = Toast.makeText(getApplicationContext(), "You scored a " + score + "\nNot bad!", Toast.LENGTH_LONG);
+            toast.show();
+        }
+            /**
+             * If the user scores <= 70, the following toast will appear on screen
+             */
+            else {
+            Toast toast = Toast.makeText(getApplicationContext(), "You scored a " + score + "\nTry again Mudblood.", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        /**
+         * Share your score with an external app
+         */
+        Intent sharingIntent = new Intent();
+        sharingIntent.setAction(Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        sharingIntent.putExtra(Intent.EXTRA_TEXT,"I scored " + score + " on the Harry Potter Quiz App!");
+        startActivity(Intent.createChooser(sharingIntent,"Share your score!"));
 
         clearQuizQuestions();
     }
 
+    /**
+     * Method to tally up user's score
+     */
     private int calculateScore(boolean answerOne, boolean answerTwo, boolean answerThree, boolean answerFour, boolean answerFiveA, boolean answerFiveB, boolean answerFiveC, boolean answerFiveD, boolean answerSix, boolean answerSeven, boolean answerEight, boolean answerNine, String answerTen) {
         int score = 0;
 
@@ -199,6 +238,9 @@ public class MainActivity extends AppCompatActivity {
         return score;
     }
 
+    /**
+     * Method to clear quiz questions
+     */
     private void clearQuizQuestions() {
         RadioGroup questionOneRadioGroup = (RadioGroup) findViewById(R.id.question_one_radio_group);
         questionOneRadioGroup.clearCheck();
